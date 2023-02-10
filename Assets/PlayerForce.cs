9c5098e435a5bec65065a5f98febee1ba860fc74
SomnusKL <117642820+SomnusKL@users.noreply.github.com>
2023-02-10 16:44:58 -0500

    leg and arm movements separately

---
+++ Assets/PlayerForce.cs
@@ -0,0 +1,73 @@
+using System.Collections;
+using System.Collections.Generic;
+using JetBrains.Annotations;
+using UnityEngine;
+
+public class PlayerForce : MonoBehaviour
+{
+    public Rigidbody2D armleft;
+    public Rigidbody2D armright;
+    public Rigidbody2D legleft;
+    public Rigidbody2D legright;
+    public Rigidbody2D body;
+    
+    public AudioSource mysource;
+    public AudioClip jumpClip;
+    Rigidbody2D mainBody;
+    public float power = 2f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        mainBody = GetComponent<Rigidbody2D>();
+        mysource.PlayOneShot(jumpClip);
+        mysource.clip = jumpClip;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            armleft.AddForce(transform.right*power, ForceMode2D.Impulse);
+            mysource.Play();
+        }
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            armright.AddForce(transform.right*-power, ForceMode2D.Impulse);
+            mysource.Play();
+        }
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
+            legleft.AddForce(transform.right*power, ForceMode2D.Impulse);
+            mysource.Play();
+        }
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            legright.AddForce(transform.right*-power, ForceMode2D.Impulse);
+            mysource.Play();
+        }
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            body.AddForce(transform.up*power, ForceMode2D.Impulse);
+            mysource.Play();
+        }
+        /*
+        if (Input.GetKey(KeyCode.W))
+        {
+            mainBody.velocity = new Vector3(0, power, 0);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            mainBody.velocity = new Vector3(0, -power, 0);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            mainBody.velocity = new Vector3(-power, 0, 0);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            mainBody.velocity = new Vector3(power, 0, 0);
+        }*/
+    }
+}
